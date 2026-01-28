747e269b5dfe65114475108ca3d81db077d45e84
47arsh <arxhx7@gmail.com>
2026-01-28 15:15:08 +0530

    Implement all models

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 const app = express();
 
 app.use(
@@ -15,5 +14,10 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true, limit : "16kb"}))
 app.use(express.static("public"))
 
+//import routes
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+
+//routes
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export {app};
