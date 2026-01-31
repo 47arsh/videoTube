29187ec38e5caa8d0c2a1cc01238a15325890c92
47arsh <arxhx7@gmail.com>
2026-01-31 12:40:13 +0530

    implement logout controller and route

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -200,7 +200,30 @@ const loginUser = asyncHandler( async (req,res) => {
 })
 
 const logoutUser = asyncHandler( async (req,res) => {
-  
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set : { 
+        refreshToken : undefined
+      }
+    },
+    {new : true}
+  )
+  const options = {
+    httpOnly : true,
+    secure : process.env.NODE_ENV==="production"
+  }
+  return res
+          .status(200)
+          .clearCookie("accessToken",options)
+          .clearCookie("refreshToken",options)
+          .json(
+            new ApiResponse(
+              200,
+              {},
+              "User logged out successfully"
+            )
+          )
 })
 
 const refreshAccessToken = asyncHandler(async (req,res) => {
@@ -263,5 +286,6 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
 export{
   registerUser,
   loginUser,
-  refreshAccessToken
+  refreshAccessToken,
+  logoutUser,
 }
